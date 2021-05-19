d9210be2bed91358e2bbf151bb98720a13198cc6
tiendat19521345 <71374867+tiendat19521345@users.noreply.github.com>
2021-05-19 14:57:21 +0700

    Giao diện Login và Signup
    Giao diện nha

---
+++ RestaurantManager/fSignup.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace RestaurantManager
+{
+    public partial class fSignup : Form
+    {
+        public fSignup()
+        {
+            InitializeComponent();
+        }
+
+        private void buttonClose_Click(object sender, EventArgs e)
+        {
+            this.Close();
+            new fLogin().Show();
+        }
+    }
+}

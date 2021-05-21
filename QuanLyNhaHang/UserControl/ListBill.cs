d4b681a8d75c6ab8241f5c60682a78bc20c45954
HoQuangLinh <hoquanglinh2710@gmail.com>
2021-05-21 16:45:36 +0700

    Tao ra man hinh chinh cho phan mem

---
+++ QuanLyNhaHang/UserControl/ListBill.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using iTextSharp.text;
+using iTextSharp.text.pdf;
+using System.IO;
+
+namespace QuanLyNhaHang
+{
+    public partial class ListBill : UserControl
+    {
+        public ListBill()
+        {
+            InitializeComponent();
+           
+        }
+            
+    }
+}
---
+++ QuanLyNhaHang/UserControl/ListFood.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using Bunifu.Framework.Lib;
+using Bunifu.Framework.UI;
+using Bunifu;
+using BunifuAnimatorNS;
+using System.Data.SqlClient;
+
+namespace QuanLyNhaHang
+{
+    public partial class ListFood : UserControl
+    {
+        public ListFood()
+        {
+            InitializeComponent();
+
+            cbbSelect.SelectedIndex = 0;
+
+        }
+
+
+
+    } 
+}
